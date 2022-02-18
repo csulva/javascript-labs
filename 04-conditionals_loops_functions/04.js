871d9e849c68c6dbccee69793d18eeeba3534147
csulva <chrissulva1@gmail.com>
2022-02-18 13:15:39 -0500

    finish 04

--- 04-conditionals_loops_functions/04.js
+++ 04-conditionals_loops_functions/04.js
@@ -22,20 +22,57 @@
 
 // Write your code here:
 
+// 1.  0K − 273.15 = -273.1°C
 
+function kToC(k) {
+    let c = k - 273.15;
+    return c;
+}
+function cToK(c) {
+    let k = c + 273.15;
+    return k;
+}
 
+//  (0K − 273.15) × 9/5 + 32 = -459.7°F
+function kToF(k) {
+    let f = (k - 273.15) * 9/5 + 32;
+    return f;
+}
+// (32°F − 32) × 5/9 + 273.15 = 273.15K
+function fToK(f) {
+    let k = (f - 32) * 5/9 + 273.15
+    return k;
+}
 
+// a^2 + b^2 == c^2
+function pythagoras(a, b) {
+    let c = (a ** 2 + b ** 2) ** (1/2);
+    return c;
+}
 
-
-
-
+function MToKm(m) {
+    let km = m * 1.609;
+    return km
+}
+function KmToM(k) {
+    let m = k / 1.609;
+    return m
+}
+function inchToCm(inch) {
+    let cm = inch * 2.54;
+    return cm
+}
+function CmToInch(cm) {
+    let inch = cm / 2.54;
+    return inch
+}
 
 // Do not modify anything below this line
 
-console.log(KtoC(272));
-console.log(CtoK(0));
-console.log(FtoK(30));
-console.log(KtoF(100));
+console.log(kToC(272));
+console.log(cToK(0));
+console.log(fToK(30));
+console.log(kToF(100));
 
 console.log(pythagoras(10, 20));
 console.log(pythagoras(30, 100));
