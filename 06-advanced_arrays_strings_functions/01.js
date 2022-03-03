44a098c16cda990f6cb816c2f8f64dae77bd88a3
csulva <chrissulva1@gmail.com>
2022-03-03 14:06:51 -0500

    first attempt

--- 06-advanced_arrays_strings_functions/01.js
+++ 06-advanced_arrays_strings_functions/01.js
@@ -9,3 +9,69 @@
  * 5. Refactor: define the sum, count and average function as a separate
  *    function and pass it to the forEach functions as a name.
  */
+
+// 1. Use a random number generator to generate an array of 100 values.
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100 + 1);
+}
+randomNumbers = [];
+for (i = 0; i < 100; i++) {
+  x = getRandomNumber();
+  randomNumbers.push(x);
+}
+
+// 2. Use forEach on the array to output the sum, count and average.
+let sum = 0;
+let count = 0;
+randomNumbers.forEach((num) => (sum += num));
+randomNumbers.forEach((num) => (count += 1));
+console.log(sum);
+console.log(count);
+let average = sum / count;
+console.log(average);
+
+// 3. Use forEach to create a new array from the first one that halves all the
+//    values.
+
+let halves = [];
+randomNumbers.forEach((num) => {
+  halves.push(num / 2);
+});
+console.log(randomNumbers[0]);
+console.log(halves[0]);
+
+// 4. Use forEach on the new array to output the sum, count and average again.
+sum = 0;
+count = 0;
+halves.forEach((num) => {
+  sum += num;
+  count += 1;
+});
+console.log(sum);
+console.log(count);
+average = sum / count;
+console.log(average)
+
+// 5. Refactor: define the sum, count and average function as a separate
+//    function and pass it to the forEach functions as a name.
+sum = 0;
+function calculateSum(num) {
+    sum += num;
+    return sum;
+}
+function doCount(counter=0){
+    counter += 1;
+    return counter;
+}
+
+function getAverage(sum, count) {
+    return sum/count;
+}
+let bigSum = 0;
+bigSum = randomNumbers.forEach(calculateSum());
+console.log(bigSum)
+let count2 = 0;
+count2 = randomNumbers.forEach(doCount());
+let avg2 = 0;
+avg2 = randomNumbers.forEach(getAverage(bigSum, count2));
+console.log(avg2);
